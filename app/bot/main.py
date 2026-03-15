ac57e53d8f2c4bca0d88718a19441a681ea55761
michel <mddpsjfe@gmail.com>
2026-03-15 10:59:52 -0300

    feat: adicionado o bloco responsável pela coleta de mensagens em canais

--- app/bot/main.py
+++ app/bot/main.py
@@ -14,12 +14,26 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print("================================")
+    print("=======================================")
     print(f"🤖 Sistema inicializado com sucesso!")
     print(f"👤 Logado como: {bot.user}")
     print(f"🆔 ID do Bot: {bot.user.id}")
     print("=======================================")
     print("Aguardando eventos do servidor...")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    usuario = message.author.name
+    canal = message.channel.name
+    texto = message.content
+
+    print(f'[{canal}] {usuario} disse: {texto}')
+
+    await bot.process_commands(message)
+
 
 if __name__ == "__main__":
     token = os.getenv('DISCORD_TOKEN')
